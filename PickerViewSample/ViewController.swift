13f8b853598d52c22255ed23b9ffcf9e430905e3
agreyson <amgreyson@gmail.com>
2019-10-03 16:57:45 -0700

    Initial commit

--- PickerViewSample/ViewController.swift
+++ PickerViewSample/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self        // Do any additional setup after loading the view.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 10
+        } else if component == 1 {
+            return 20
+        } else if component == 2 {
+            return 40
+        } else {
+            return 100
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return "First \(row)"
+        } else if component == 1 {
+            return "Second \(row)"
+        } else if component == 2 {
+            return "Third \(row)"
+        } else {
+            return "Fourth \(row)"
+        }
+    }
 }
 
